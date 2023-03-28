9d5ad15622e47cc76c20815e8860654e3b70f20d
rbiotblbk <97528860+rbiotblbk@users.noreply.github.com>
2023-03-28 11:19:05 +0200

    Add files via upload

---
+++ 99_Exercises/02_Exercise/02_Exercise.py
@@ -0,0 +1,35 @@
+# Online Shop
+
+""" 
+Conditions:
+~~~~~~~~~~~~
+- age : >= 18
+- payment_method : True
+- items: Product count > 0  -> warenkorb
+
+
+
+# Exercise 1: 
+- Show signle message of the missing condition
+
+# Exercise 2: 
+- Show ALLLLL Messages of ALLL Missing conditions
+
+
+# Messages:
+- Du bist zu jung
+- BZ Methode nicht vorhanden
+- Du muss Produkte auswählen
++ Zur Kasse bitte
+
+"""
+
+
+# Start Code
+age = 17  
+payment_method  = False  
+items = 0
+
+
+
+# Your code
