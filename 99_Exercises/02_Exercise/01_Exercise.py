65ef6583691de199018234bf6e4b2df343d1ebf9
rbiotblbk <97528860+rbiotblbk@users.noreply.github.com>
2023-03-28 13:47:55 +0200

    Add files via upload

---
+++ 99_Exercises/02_Exercise/01_Exercise.py
@@ -0,0 +1,17 @@
+""" 
+Welcome to TELEKOM
+~~~~~~~~~~~~~~~~~~
+
+Language/Sprache
+~~~~~~~~~~~~~~~~
+[1] EN
+[2] DE
+[3] SP
+
+Your choice> 1
+
+What is your first name? thomas
+What is your last name ? meier
+
+Welcome Customer, Thomas MEIER!
+"""
---
+++ 99_Exercises/02_Exercise/03_Exercise.py
@@ -0,0 +1,24 @@
+# E1: Sum of all numbers between 10 and 20 
+
+
+
+
+
+# E2: Sum of all numbers between (lower_limit) and (upper_limit)
+lower_limit = input()  # 2   
+upper_limit = input()  # 5   
+
+
+# E3: Get 5xnumbers from the user --> 
+""" 
+Enter your number : 5
+Enter your number : -1
+Enter your number : 3
+Enter your number : 4
+Enter your number : -2
+
+
+All numbers are : 5 -1 3 4 -2
+All pos. numbers are:  5 3 4
+All neg. numbers are: -1 -2
+"""
