059e455df45a4b386fdf0715d792d6edc05a914e
rbiotblbk <97528860+rbiotblbk@users.noreply.github.com>
2023-03-29 14:03:33 +0200

    Add files via upload

---
+++ 99_Exercises/03_Exercise/01_Exercise.py
@@ -0,0 +1,58 @@
+# While loop
+
+
+#################################################################
+# E1: Get different numbers from the user till 0 (exit point) --> 
+#################################################################
+
+""" 
+Enter your number : 5
+Enter your number : -1
+Enter your number : 3
+Enter your number : 4
+Enter your number : -2
+......
+Enter your number : 0   -> exit point
+
+
+
+All numbers are : 5 -1 3 4 -2
+All Even numbers are:  4 -2
+All Odd numbers are: 5 -1 3
+"""
+
+
+#################################################################
+# E2: Restaurant
+#################################################################
+
+"""
+Willkommen bei ACASA-Restaurant
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Menu
+~~~~
+Pizza Margeritta
+Pizza Tunfisch
+Salat Normal
+Salat VIP
+
+
+Wünschen:
+~~~~~~~~
+> Pizza Margeritta
+> Pizza Tunfisch
+> Salat VIP
+> ...
+> 0     -> exit point
+
+
+Quittung(Auflistung):
+~~~~~~~
+Pizza Margeritta
+Pizza Tunfisch
+Salat VIP
+
+Vielen Dank für Ihren Besuch.!
+
+"""
---
+++ 99_Exercises/03_Exercise/02_Excersie.py
@@ -0,0 +1,41 @@
+# E1: Find  the items smaller that 10 
+numbers = [1,4,18,3,15,2]
+
+
+# E2:  Remove the duplicates
+numbers = [1,4,18,3,15,2,1,18,4]
+
+
+# E3: Get only the even numbers from the list
+numbers = [1,4,18,3,15,2,1,18,4]
+
+
+# E4: Get the matched items between the two lists -> common values between the two lists
+numbers1 = [1,4,18,3,15,2,1,18,4]
+numbers2 = [69,1,15,3,7,90,10]
+
+#############################
+# E5: While + List 
+
+""" 
+Enter your number : 5
+Enter your number : -1
+Enter your number : 3
+Enter your number : 4
+Enter your number : -2
+......
+Enter your number : 0   -> exit point
+
+
+
+All numbers are : -2 -1 3 4 5
+Sum of all numbers: ....
+The Min number :  -2
+The max numbers: 5
+
+
+
+!!!!!!! Achtung Achtung::: sum(), min(), max()  sind nicht erlaubt!
+"""
+
+
