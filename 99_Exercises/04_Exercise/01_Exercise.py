e83d599c5b272a7f79a0a0e82cf475fd56ca9cb8
rbiotblbk <97528860+rbiotblbk@users.noreply.github.com>
2023-03-30 13:08:43 +0200

    Add files via upload

---
+++ 99_Exercises/04_Exercise/01_Exercise.py
@@ -0,0 +1,50 @@
+# E1: Sum of all values  --> output: 600
+mydict = {
+    "val1": 100,
+    "val2": 200,
+    "val3": 300,
+}
+
+
+
+# E2: Remove the key which was defined by user
+
+user_input= input("Which Key: ")
+
+user = {"id": 100, 
+        "first_name": "Thomas", 
+        "age": 55, 
+        "anwesend": True}
+
+
+# E3: Multiply of all values  --> output: 6
+mydict = {
+    "val1": 1,
+    "val2": 2,
+    "val3": 3,
+}
+ 
+
+# E4:   NATO Alphabet: The user enters his username, and the python program retrieves the NATO alphabet for the username. 
+# It a charachter not found, show the user 'Not found'
+""" 
+A: Alpha
+B: Bravo
+C: Charlie
+D: Delta
+E: Echo
+F: Foxtrott
+...
+"""
+
+##### The simulation
+""" 
+user name: ABDEG
+
+NATO Alphabet:
+A: Alpha
+B: Bravo
+D: Delta
+E: Echo
+G: Not found
+"""
