c222232b4ad6a89fcc9f6fac5faf7c9461112136
rbiotblbk <97528860+rbiotblbk@users.noreply.github.com>
2023-03-30 13:23:26 +0200

    Add files via upload

--- 03_DataStructures/03_LoopOverlist.py
+++ 03_DataStructures/03_LoopOverlist.py
@@ -6,44 +6,45 @@ print()
 
 
 
+################################################
+
+teilnehmende_list = ["Thomas", "Ingo", "Sara"]
+
+for tn in enumerate(teilnehmende_list):   # tn: Tuple (index, value)
+    print(tn) # (1, 'Ingo')
+print()
 
 
+for tn in enumerate(teilnehmende_list):   # tn: Tuple (index, value)
+    print("Index:", tn[0], "Value:", tn[1]) # (1, 'Ingo')
 
 
+# enumerate with unpacking 
+for index, value in enumerate(teilnehmende_list):
+    print(index, value) 
 
 
+########################################################
+# Explanation of enumerate--->
+
+teilnehmende_list = ["Thomas", "Ingo", "Sara"]
+
+# enumerate() --
+for idx, val in [ (0 , "Thomas") , (1,"Ingo" ), (2 ,"Sara") ]:
+    print(idx, val)
+
+print()
+############################
+# Change the start value of the index instead of 0 --> some number
+
+teilnehmende_list = ["Thomas", "Ingo", "Sara"]
+
+for index, value in enumerate(teilnehmende_list, 100):
+    print(f"{index}. {value}")
 
 
+########################################################
+user = [ "Thomas", ("VW", "Car"), True, 45 , ["A", "B"] ]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# enumerate --> Tuple (Index, Value)
-for teilnehmende in enumerate(teilnehmende_list):
-    print(teilnehmende) # --> tuple (0, 'Thomas')
-
-
-#########################
-
-
-for teilnehmende in enumerate(teilnehmende_list):
-    print(teilnehmende[0] , teilnehmende[1])
-
-
-#########################
-# Unpacking
-
-# to be continued
+for index, value in enumerate(user):
+    print(index, value)
