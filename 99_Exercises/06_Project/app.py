39abc905fe2d81a1eee2a35744c2331996186f14
rbiotblbk <97528860+rbiotblbk@users.noreply.github.com>
2023-04-03 14:03:04 +0200

    Add files via upload

---
+++ 99_Exercises/06_Project/app.py
@@ -0,0 +1,17 @@
+""" 
+Extend the solution of PDF Merger to cover the following
+
+1. The app should merge ALLLLL pdf files which are located in a folder called "data"
+2. The result(merged) file, should be stored in a folder called "output"
+
+Advanced Tasks
+~~~~~~~~~~~~~~
+3. Each merge execution, should increment the name of the result.pdf file with one
+--- result1.pdf
+--- result2.pdf
+--- result3.pdf
+
+
+
+
+"""
